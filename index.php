b933a03a5eefe1d52cbee880d481c7d4591108ef
Jeremy Denis <jeremy.denis@gmail.com>
2012-11-14 00:13:32 +0100

    First commit of this project on GitHub

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
